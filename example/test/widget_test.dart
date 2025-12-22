3308c0d666272ee98b2fc20d53d40e06a29efa95
AbirAhsan <abirahsan122@gmail.com>
2025-12-22 01:05:26 -0500

    fixed test file

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -6,7 +6,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Home screen displays all example cards', (
